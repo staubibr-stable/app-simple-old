7381e29a69838e64a79a9fbfabfa8229cff03220
staubibr <staubin.bruno@gmail.com>
2021-10-28 20:59:06 -0400

    removed unused URLs minor update to tutorial

--- main.js
+++ main.js
@@ -6,13 +6,9 @@ import Application from "./application.js";
 Core.WaitForDocument().then(Start);
 
 function Start() {	
-
 	var path = location.href.split("/").slice(0,-2).join("/");
 
 	streamSaver.mitm = path + "/api-web-devs/references/StreamSaver/mitm.html";
-	
-	// Core.URLs.conversion = "http://localhost:8080/parser/auto";
-	// Core.URLs.models = "http://localhost/Dev/arslab-logs/devs-logs/";
 	
 	var app = new Application(document.body);
 }
